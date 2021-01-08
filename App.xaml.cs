0be8400eeb6684f03e1fc37d7830c0b89c10c097
老马不识途 <WangJianjun1994@live.com>
2021-01-08 15:04:36 +0800

    init Prj

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using ESRI.ArcGIS.esriSystem;
+
+namespace AEMapWPF
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.Engine);
+            InitializeEngineLicense();
+        }
+        private void InitializeEngineLicense()
+        {
+            AoInitialize aoi = new AoInitializeClass();
+            esriLicenseProductCode productCode = esriLicenseProductCode.esriLicenseProductCodeEngine;
+            if (aoi.IsProductCodeAvailable(productCode) == esriLicenseStatus.esriLicenseAvailable)
+            {
+                aoi.Initialize(productCode);
+            }
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.SystemUI;
+using System.Windows.Forms;
+
+namespace AEMapWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AxMapControl mapControl = null;
+        AxTOCControl tocControl = null;
+        AxToolbarControl toolbarControl = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateEngineControls();
+        }
+        //建立AE控件与WindowsFormsHost控件的联系
+        private void CreateEngineControls()
+        {
+            mapControl = new AxMapControl();
+            windowsFormsHost3.Child = mapControl;
+            tocControl = new AxTOCControl();
+            windowsFormsHost2.Child = tocControl;
+            toolbarControl = new AxToolbarControl();
+            windowsFormsHost1.Child = toolbarControl;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tocControl.SetBuddyControl(mapControl);
+            toolbarControl.SetBuddyControl(mapControl);
+            ToolbarAddItems();
+        } //ToolBar中添加工具
+        private void ToolbarAddItems()
+        {
+            toolbarControl.AddItem(new ControlsOpenDocCommandClass(), -1, -1, false, -1, esriCommandStyles.esriCommandStyleIconOnly);
+            toolbarControl.AddItem(new ControlsSaveAsDocCommandClass(), -1, -1, false, -1, esriCommandStyles.esriCommandStyleIconOnly);
+            toolbarControl.AddItem(new ControlsAddDataCommandClass(), -1, -1, false, -1, esriCommandStyles.esriCommandStyleIconOnly);
+            toolbarControl.AddItem("esriControls.ControlsMapNavigationToolbar");
+            toolbarControl.AddItem("esriControls.ControlsMapIdentifyTool");
+        }
+
+    }
+}
